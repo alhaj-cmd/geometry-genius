18d59c76fb0d3bab8e44725dace98a53f6f9a49d
github_username <you@domain.com>
2023-02-18 22:47:39 +0600

    complete assignment

--- js/card.js
+++ js/card.js
@@ -6,17 +6,19 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
     const triangleFieldValue = document.getElementById('baseline-Field');
     const triangleField = triangleFieldValue.value;
     const baselineFieldValue = document.getElementById('height-Field');
-    const baselineField =baselineFieldValue.value;
+    const baselineField = baselineFieldValue.value;
     const triangleName = document.getElementById('triangle-name').innerText;
 
     // validation
-    if (triangleField === '' || baselineField === '' || triangleField <= 0 || baselineField <= 0) {
-        return alert('please enter number')
-    }
+    if (triangleField > 0 && baselineField > 0) {
 
-    const triangleAreaCalculate = 0.5 * triangleField * baselineField;
-    const triangleAreaTotal = parseInt(triangleAreaCalculate);
-    displayData(number, triangleName, triangleAreaTotal)
+        const triangleAreaCalculate = 0.5 * triangleField * baselineField;
+        const triangleAreaTotal = parseInt(triangleAreaCalculate);
+        displayData(number, triangleName, triangleAreaTotal)
+    }
+    else {
+        alert('please enter number')
+    }
     triangleFieldValue.value = '';
     baselineFieldValue.value = '';
 
@@ -30,17 +32,21 @@ document.getElementById('btn-rectangle').addEventListener('click', function () {
     const rectangleField1 = document.getElementById('rectangleFieldOne');
     const rectangleFieldOne = rectangleField1.value;
     const rectangleField2 = document.getElementById('rectangleFieldTwo');
-    const rectangleFieldTwo =rectangleField2.value;
+    const rectangleFieldTwo = rectangleField2.value;
     const rectangleNameText = document.getElementById('rectangle-name').innerText;
     // validation
-    if (rectangleFieldOne === '' || rectangleFieldTwo === '' || rectangleFieldOne <= 0 || rectangleFieldTwo <= 0) {
-        return alert('please enter number')
+    if (rectangleFieldOne > 0 && rectangleFieldTwo > 0) {
+
+        const rectangleCalculate = rectangleFieldOne * rectangleFieldTwo;
+        const rectangleCalculateNumber = parseFloat(rectangleCalculate);
+        displayData(number, rectangleNameText, rectangleCalculateNumber);
     }
 
-    const rectangleCalculate = rectangleFieldOne * rectangleFieldTwo;
-    const rectangleCalculateNumber = parseFloat(rectangleCalculate);
-    displayData(number, rectangleNameText, rectangleCalculateNumber);
-    rectangleField1.value =''
+    else {
+        alert('please enter number')
+    }
+
+    rectangleField1.value = ''
     rectangleField2.value = ''
 
 
@@ -51,20 +57,25 @@ document.getElementById('btn-rectangle').addEventListener('click', function () {
 document.getElementById('btn-parallelogram').addEventListener('click', function () {
     number += 1;
     const parallelogramField1 = document.getElementById('parallelogramFieldOne');
-    const parallelogramFieldOne =  parallelogramField1.value;
+    const parallelogramFieldOne = parallelogramField1.value;
     const parallelogramField2 = document.getElementById('parallelogramFieldTwo');
-    const parallelogramFieldTwo =parallelogramField2.value;
+    const parallelogramFieldTwo = parallelogramField2.value;
     const parallelogramNameText = document.getElementById('parallelogram-name').innerText;
 
-    if (parallelogramFieldOne === '' || parallelogramFieldTwo === '' || parallelogramFieldOne <= 0 || parallelogramFieldTwo <= 0) {
-        return alert('please enter number')
-    }
-    const parallelogramCalculate = parallelogramFieldOne * parallelogramFieldTwo;
-    const parallelogramCalculateTotal = parseFloat(parallelogramCalculate);
-    displayData(number, parallelogramNameText, parallelogramCalculateTotal)
+    if (parallelogramFieldOne > 0 && parallelogramFieldTwo > 0) {
 
-    parallelogramField1.value =''
-    parallelogramField1.value =''
+        const parallelogramCalculate = parallelogramFieldOne * parallelogramFieldTwo;
+        const parallelogramCalculateTotal = parseFloat(parallelogramCalculate);
+        displayData(number, parallelogramNameText, parallelogramCalculateTotal)
+    }
+
+    else {
+        alert('please enter number')
+    }
+
+
+    parallelogramField1.value = ''
+    parallelogramField2.value = ''
 
 
 })
@@ -75,16 +86,20 @@ document.getElementById('btn-rhombusField').addEventListener('click', function (
     const rhombusField1 = document.getElementById('rhombusFieldOne');
     const rhombusFieldOne = rhombusField1.value;
     const rhombusField2 = document.getElementById('rhombusFieldTwo');
-    const rhombusFieldTwo =rhombusField2.value;
+    const rhombusFieldTwo = rhombusField2.value;
     const rhombusName = document.getElementById('rhombus-name').innerText;
     // validation
-    if (rhombusFieldOne === '' || rhombusFieldTwo === '' || rhombusFieldOne <= 0 || rhombusFieldTwo <= 0) {
-        return alert('please enter number')
+    if (rhombusFieldOne > 0 && rhombusFieldTwo > 0) {
+
+        const rhombusCalculate = 0.5 * rhombusFieldOne * rhombusFieldTwo;
+        const rhombusCalculateTotal = parseInt(rhombusCalculate);
+        displayData(number, rhombusName, rhombusCalculateTotal);
     }
 
-    const rhombusCalculate = 0.5 * rhombusFieldOne * rhombusFieldTwo;
-    const rhombusCalculateTotal = parseInt(rhombusCalculate);
-    displayData(number, rhombusName, rhombusCalculateTotal);
+    else {
+        alert('please enter number')
+    }
+
     rhombusField1.value = ''
     rhombusField2.value = ''
 
@@ -100,13 +115,17 @@ document.getElementById('btn-pentagon').addEventListener('click', function () {
     const pentagonFieldTwo = pentagonField2.value;
     const pentagonName = document.getElementById('pentagon-name').innerText;
     // validation
-    if (pentagonFieldOne === '' || pentagonFieldTwo === '' || pentagonFieldOne <= 0 || pentagonFieldTwo <= 0) {
-        return alert('please enter number')
+    if (pentagonFieldOne > 0 && pentagonFieldTwo > 0) {
+
+        const pentagonCalculate = 0.5 * pentagonFieldOne * pentagonFieldTwo;
+        const pentagonCalculateTotal = parseFloat(pentagonCalculate);
+        displayData(number, pentagonName, pentagonCalculateTotal);
     }
 
-    const pentagonCalculate = 0.5 * pentagonFieldOne * pentagonFieldTwo;
-    const pentagonCalculateTotal = parseFloat(pentagonCalculate);
-    displayData(number, pentagonName, pentagonCalculateTotal);
+    else {
+        alert('please enter number')
+    }
+
     pentagonField1.value = ''
     pentagonField2.value = ''
 
@@ -117,23 +136,24 @@ document.getElementById('btn-pentagon').addEventListener('click', function () {
 document.getElementById('btn-ellipse').addEventListener('click', function () {
     number += 1;
     const ellipseFieldOne = document.getElementById('ellipseFieldOne')
-    const ellipseFieldOneValue =ellipseFieldOne.value;
+    const ellipseFieldOneValue = ellipseFieldOne.value;
     const ellipseFieldTwo = document.getElementById('ellipseFieldTwo');
     const ellipseFieldTwoValue = ellipseFieldTwo.value;
- 
+
     const ellipseName = document.getElementById('ellipse-name').innerText;
     // validation
-    if (ellipseFieldOneValue === '' || ellipseFieldTwoValue === '' || ellipseFieldOneValue === null || ellipseFieldTwoValue === null || ellipseFieldOneValue <= 0 || ellipseFieldTwoValue <= 0) {
-        return alert('please enter number')
+    if (ellipseFieldOneValue > 0 && ellipseFieldTwoValue > 0) {
+
+        const ellipseCalculate = 3.14159 * ellipseFieldOneValue * ellipseFieldTwoValue;
+        const ellipseCalculateTotal = ellipseCalculate.toFixed(2);
+        displayData(number, ellipseName, ellipseCalculateTotal)
     }
-  
 
-    const ellipseCalculate = 3.14159 * ellipseFieldOneValue * ellipseFieldTwoValue;
-    const ellipseCalculateTotal = ellipseCalculate.toFixed(2);
-    displayData(number, ellipseName, ellipseCalculateTotal)
+    else {
+        alert('please enter number')
+    }
 
-    // ellipseCalculate.value = '';
-    ellipseFieldOne.value  = ' ';
+    ellipseFieldOne.value = ' ';
     ellipseFieldTwo.value = ' ';
 
 })
--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 function displayData(serial, sixcardName, totalCardCalculation) {
     const calculation = document.getElementById('calculation');
-    const tr = document.createElement("tr");
+    // document.getElementById("#myId").classList.add('myClass')
+    var tr = document.createElement("tr");
+    // const div = document.classList(div.name);
+
     tr.innerHTML = `
-    <div>
     <td>${serial}</td>    
     <td>${sixcardName}</td>    
-    <td>${totalCardCalculation}cm<sup>2</sup> </td>
-    <button>submit</button>   
-    </div>
-     
+    <td>${totalCardCalculation}cm<sup>2</sup> </td> 
+    <button>Covert<sup>2</sup></button> 
     
     `
     calculation.appendChild(tr);
@@ -16,13 +16,13 @@ function displayData(serial, sixcardName, totalCardCalculation) {
 
 // card uses in random color
 
-randomColor = function(e) {
-    e.style.background =  "#" + (Math.random() * 0xFFFFFF<<0).toString(16);
+randomColor = function (e) {
+    e.style.background = "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
 }
 
 // question file add in js
 
 document.getElementById("btn-submit").onclick = function () {
-//     console.log('hello')
-    window.location.href= "http://127.0.0.1:5500/question.html";
+    //     console.log('hello')
+    window.location.href = "http://127.0.0.1:5500/question.html";
 };
