3d66f1654bbdacfef249f59bc71faf9b0a288f4e
github_username <you@domain.com>
2023-02-18 06:24:56 +0600

    calculate card

--- card.js
+++ card.js
@@ -1,32 +1,30 @@
 // console.log('card')
+let number = 0;
 document.getElementById('btn-triangle').addEventListener('click', function(){
+    number +=1;
     const triangleField =document.getElementById('baseline-Field').value;
-    // const triangleFieldValue = triangleField.value;
-    // console.log(triangleField);
-    // const triangleFieldString = parseFloat(triangleFieldValue);
-    // // triangleField.value=triangleFieldString;
+  
 
     const baselineField = document.getElementById('height-Field').value;
     const triangleName = document.getElementById('triangle-name').innerText;
-    // const baselineFieldValue = baselineField.value;
-    // const baselineFieldString = parseFloat(baselineFieldValue);
-    // console.log(baselineFieldString);
-    // // baselineField.value = baselineFieldString;
 
     const triangleArea = 0.5*triangleField*baselineField;
-    console.log(triangleArea,triangleName);
-    const calculation =document.getElementById('calculation')
-    // const calculationFiled =calculation.innerText;
+    const triangleAreaParse = parseInt(triangleArea);
+    // console.log(triangleArea,triangleName);
+    displayData( triangleAreaParse, triangleName)
+    
+
+})
+ 
+function displayData(triangleAreaParse, triangleName){
+    const calculation =document.getElementById('calculation');
     const tr= document.createElement("tr");
     tr.innerHTML= `
-    <td>${1}</td>
-    <td>${triangleArea}</td>
-    <td>${1}</td>
-    <td>${1}</td>
-    <h1>{name}</h1>
-    </div>
+    <td>${1}</td>    
+    <td>${triangleName}</td>    
+    <td>${triangleAreaParse}</td>cm<sup>2</sup>    
+     
     
     `
     calculation.appendChild(tr);
-
-})
+}
